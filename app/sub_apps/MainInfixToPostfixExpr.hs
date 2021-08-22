e72faf786a1d4fb2f4c15426a6dd15bf7431c6f8
Skyf0l <tom.rorato@epitech.eu>
2021-08-22 17:52:46 +0200

    Add handleExitProgram to InfixToPostfixExpr main

--- app/sub_apps/MainInfixToPostfixExpr.hs
+++ app/sub_apps/MainInfixToPostfixExpr.hs
@@ -1,11 +1,17 @@
 module MainInfixToPostfixExpr where
 
+import HandleExitProgram
+  ( ExitProgram (..),
+    exitWith,
+    handleExitProgram,
+    throw,
+  )
 import InfixToPostfixExpr (infixToPostfixExpr)
 import System.Environment (getArgs)
 
 main :: IO ()
-main = do
+main = handleExitProgram $ do
   args <- getArgs
   case args of
     [expr] -> putStrLn $ infixToPostfixExpr expr
-    _ -> putStrLn "Usage: ./funInfixToPostfixExpr <expr>"
+    _ -> throw $ ExitProgram 84 "Usage: ./funInfixToPostfixExpr <expr>"

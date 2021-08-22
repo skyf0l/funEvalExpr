2cf85acd1751f1dc70f89dae613942ef55bf55dd
Skyf0l <tom.rorato@epitech.eu>
2021-08-22 16:57:11 +0200

    Add read and show instances of operator

--- src/Operators.hs
+++ src/Operators.hs
@@ -9,12 +9,13 @@ module Operators
 where
 
 import Data.Data (Data, Typeable)
+import Text.Printf (printf)
 
 data UnaryOperator
   = Pos
   | Neg
   | Not
-  deriving (Eq, Show, Data, Typeable)
+  deriving (Eq, Show, Data, Typeable, Read)
 
 data BinaryOperator
   = Exp
@@ -32,34 +33,56 @@ data BinaryOperator
   | Ne
   | And
   | Or
-  deriving (Eq, Show, Data, Typeable)
+  deriving (Eq, Show, Data, Typeable, Read)
 
 data Operator
-  = UnaryOp UnaryOperator
-  | BinaryOp BinaryOperator
-  deriving (Eq, Show, Data, Typeable)
+  = OpUnaryOperator UnaryOperator
+  | OpBinaryOperator BinaryOperator
+  deriving (Eq, Data, Typeable)
+
+instance Read Operator where
+  readsPrec _ x = case reads x :: [(UnaryOperator, String)] of
+    [(operator, xs)] -> [(OpUnaryOperator operator, xs)]
+    _ -> case reads x :: [(BinaryOperator, String)] of
+      [(operator, xs)] -> [(OpBinaryOperator operator, xs)]
+      _ -> []
+
+instance Show Operator where
+  show (OpUnaryOperator op) = show op
+  show (OpBinaryOperator op) = show op
 
 data ExprElem
-  = Operator
-  | Float
-  deriving (Show)
+  = EEFloat Float
+  | EEOperator Operator
+  deriving (Eq, Data, Typeable)
+
+instance Read ExprElem where
+  readsPrec _ x = case reads x :: [(Float, String)] of
+    [(f, xs)] -> [(EEFloat f, xs)]
+    _ -> [(EEOperator operator, rest)]
+      where
+        [(operator, rest)] = reads x :: [(Operator, String)]
+
+instance Show ExprElem where
+  show (EEFloat f) = printf "%.2f" f
+  show (EEOperator op) = show op
 
 getOperatorPrecedence :: Operator -> Int
-getOperatorPrecedence (BinaryOp Exp) = 120
-getOperatorPrecedence (BinaryOp Pow) = 120
-getOperatorPrecedence (UnaryOp Pos) = 110
-getOperatorPrecedence (UnaryOp Neg) = 110
-getOperatorPrecedence (UnaryOp Not) = 110
-getOperatorPrecedence (BinaryOp Mul) = 100
-getOperatorPrecedence (BinaryOp Div) = 100
-getOperatorPrecedence (BinaryOp Mod) = 100
-getOperatorPrecedence (BinaryOp Add) = 95
-getOperatorPrecedence (BinaryOp Sub) = 95
-getOperatorPrecedence (BinaryOp Gt) = 80
-getOperatorPrecedence (BinaryOp Lt) = 80
-getOperatorPrecedence (BinaryOp Ge) = 80
-getOperatorPrecedence (BinaryOp Le) = 80
-getOperatorPrecedence (BinaryOp Eq) = 80
-getOperatorPrecedence (BinaryOp Ne) = 80
-getOperatorPrecedence (BinaryOp And) = 75
-getOperatorPrecedence (BinaryOp Or) = 70
+getOperatorPrecedence (OpBinaryOperator Exp) = 120
+getOperatorPrecedence (OpBinaryOperator Pow) = 120
+getOperatorPrecedence (OpUnaryOperator Pos) = 110
+getOperatorPrecedence (OpUnaryOperator Neg) = 110
+getOperatorPrecedence (OpUnaryOperator Not) = 110
+getOperatorPrecedence (OpBinaryOperator Mul) = 100
+getOperatorPrecedence (OpBinaryOperator Div) = 100
+getOperatorPrecedence (OpBinaryOperator Mod) = 100
+getOperatorPrecedence (OpBinaryOperator Add) = 95
+getOperatorPrecedence (OpBinaryOperator Sub) = 95
+getOperatorPrecedence (OpBinaryOperator Gt) = 80
+getOperatorPrecedence (OpBinaryOperator Lt) = 80
+getOperatorPrecedence (OpBinaryOperator Ge) = 80
+getOperatorPrecedence (OpBinaryOperator Le) = 80
+getOperatorPrecedence (OpBinaryOperator Eq) = 80
+getOperatorPrecedence (OpBinaryOperator Ne) = 80
+getOperatorPrecedence (OpBinaryOperator And) = 75
+getOperatorPrecedence (OpBinaryOperator Or) = 70

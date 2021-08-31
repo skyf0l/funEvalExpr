f179f1b3c668901c0a71d1438f2a89c8cca01f74
Skyf0l <tom.rorato@epitech.eu>
2021-08-31 23:22:56 +0200

    Update stringToInfixExpr

--- src/InfixToPostfixExpr.hs
+++ src/InfixToPostfixExpr.hs
@@ -25,18 +25,31 @@ roundHalfUp n d = fromInteger (round $ n * multiplier) / multiplier
   where
     multiplier = 10 ^ d
 
+-- op stack -> (remaining op stack, expr)
+popOpStackUntilOpen :: [ExprElem] -> ([ExprElem], [ExprElem])
+popOpStackUntilOpen [] = throw $ ExitProgram 84 "Unexpected end of expression"
+popOpStackUntilOpen (EEDelimiter Open : xs) = (xs, [])
+popOpStackUntilOpen (op : xs) = (opStack, op : expr)
+  where
+    (opStack, expr) = popOpStackUntilOpen xs
+
 -- infix expression -> operator stack -> postfix expression
 infixToPostfix :: [ExprElem] -> [ExprElem] -> [ExprElem]
 infixToPostfix [] opStack = opStack
 infixToPostfix (EEFloat f : xs) opStack = EEFloat f : infixToPostfix xs opStack
-infixToPostfix (op : xs) [] = infixToPostfix xs [op]
-infixToPostfix (EEDelimiter Open : xs) opStack = infixToPostfix xs opStack
-infixToPostfix (EEDelimiter Close : xs) opStack = infixToPostfix xs opStack
+infixToPostfix (EEDelimiter Open : xs) opStack = infixToPostfix xs opStack'
+  where
+    op = EEDelimiter Open
+    opStack' = op : opStack
+infixToPostfix (EEDelimiter Close : xs) opStack = infixToPostfix expr $ opStack' ++ opStack
+  where
+    (opStack', expr) = popOpStackUntilOpen xs
 infixToPostfix (op : xs) opStack = infixToPostfix xs $ op : opStack
 
 -- string expression -> next op is unary operator -> postfix expression
 stringToInfixExpr :: String -> Bool -> [ExprElem]
 stringToInfixExpr "" _ = []
+stringToInfixExpr (' ' : xs) isUnary = stringToInfixExpr xs isUnary
 stringToInfixExpr expr isUnary = case findNextOp expr of
   Just (opStr, restExpr) -> op : stringToInfixExpr restExpr nextIsUnary
     where

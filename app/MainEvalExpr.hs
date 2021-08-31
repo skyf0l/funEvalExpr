cd67afbc001479915aa5f15050218f878720232e
Skyf0l <tom.rorato@epitech.eu>
2021-09-01 01:09:39 +0200

    Update display of result

--- app/MainEvalExpr.hs
+++ app/MainEvalExpr.hs
@@ -8,6 +8,7 @@ import HandleExitProgram
     throw,
   )
 import InfixToPostfixExpr (infixToPostfixExpr)
+import Maths (roundHalfUp)
 import System.Environment (getArgs)
 import Text.Printf (printf)
 
@@ -15,5 +16,7 @@ main :: IO ()
 main = handleExitProgram $ do
   args <- getArgs
   case args of
-    [exprStr] -> printf "%.2f\n" $ evalPostfixExpr $ infixToPostfixExpr exprStr
+    [exprStr] -> printf "%.2f\n" res
+      where
+        res = roundHalfUp (evalPostfixExpr $ infixToPostfixExpr exprStr) 2
     _ -> throw $ ExitProgram 84 "Usage: ./funEvalExpr <expr>"
--- app/sub_apps/MainEvalPostfixExpr.hs
+++ app/sub_apps/MainEvalPostfixExpr.hs
@@ -7,6 +7,7 @@ import HandleExitProgram
     handleExitProgram,
     throw,
   )
+import Maths (roundHalfUp)
 import Operators (ExprElem)
 import System.Environment (getArgs)
 import Text.Printf (printf)
@@ -17,6 +18,8 @@ main = handleExitProgram $ do
   args <- getArgs
   case args of
     [exprStr] -> case readMaybe exprStr of
-      Just expr -> printf "%.2f\n" (evalPostfixExpr expr)
+      Just expr -> printf "%.2f\n" res
+        where
+          res = roundHalfUp (evalPostfixExpr expr) 2
       _ -> throw $ ExitProgram 84 "Can not parse expression"
     _ -> throw $ ExitProgram 84 "Usage: ./funEvalPostfixExpr <expr>"

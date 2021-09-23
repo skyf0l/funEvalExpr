7bd47d1ddc084c153808bdf09d56cad2ba3550cb
Skyf0l <tom.rorato@epitech.eu>
2021-09-23 16:29:09 +0200

    Add all operator evaluations

--- app/MainEvalExpr.hs
+++ app/MainEvalExpr.hs
@@ -63,11 +63,41 @@ maybeAstParser s = case readP_to_S (skipSpaces *> parseExpr <* eof) s of
 
 eval :: AST -> Float
 eval (Operand n) = n
-eval (Operator (BinaryOperator (Add a b))) = eval a + eval b
-eval (Operator (BinaryOperator (Sub a b))) = eval a - eval b
+eval (Operator (UnaryOperator (Pos a))) = eval a
+eval (Operator (UnaryOperator (Neg a))) = - eval a
+eval (Operator (UnaryOperator (Not (Operand 0)))) = 1
+eval (Operator (UnaryOperator (Not _))) = 0
+eval (Operator (BinaryOperator (Exp a b))) = eval a * 10 ** eval b
+eval (Operator (BinaryOperator (Pow a b))) = eval a ** eval b
 eval (Operator (BinaryOperator (Mul a b))) = eval a * eval b
 eval (Operator (BinaryOperator (Div a b))) = eval a / eval b
 eval (Operator (BinaryOperator (Mod a b))) = eval a `mod'` eval b
+eval (Operator (BinaryOperator (Add a b))) = eval a + eval b
+eval (Operator (BinaryOperator (Sub a b))) = eval a - eval b
+eval (Operator (BinaryOperator (Gt a b)))
+  | eval a > eval b = 1
+  | otherwise = 0
+eval (Operator (BinaryOperator (Lt a b)))
+  | eval a < eval b = 1
+  | otherwise = 0
+eval (Operator (BinaryOperator (Ge a b)))
+  | eval a >= eval b = 1
+  | otherwise = 0
+eval (Operator (BinaryOperator (Le a b)))
+  | eval a <= eval b = 1
+  | otherwise = 0
+eval (Operator (BinaryOperator (Eq a b)))
+  | eval a == eval b = 1
+  | otherwise = 0
+eval (Operator (BinaryOperator (Ne a b)))
+  | eval a /= eval b = 1
+  | otherwise = 0
+eval (Operator (BinaryOperator (And a b)))
+  | eval a /= 0 && eval b /= 0 = 1
+  | otherwise = 0
+eval (Operator (BinaryOperator (Or a b)))
+  | eval a /= 0 || eval b /= 0 = 1
+  | otherwise = 0
 
 main :: IO ()
 main = handleExitProgram $ do

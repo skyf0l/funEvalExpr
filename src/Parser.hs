6d4f2eff9e69210691ea6f2b274efac8a57602ac
Skyf0l <tom.rorato@epitech.eu>
2021-09-23 20:09:37 +0200

    Update binaries operator parsing

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,40 +3,124 @@ module Parser (maybeAstParser) where
 import Ast
 import LibParserCombinators
 
+-- Operand
 parseOperand :: ReadP AST
-parseOperand = Operand <$> token unsignedFloat
+parseOperand = Operand <$> token float
 
-parseOperator :: String -> (a -> a -> a) -> ReadP (a -> a -> a)
-parseOperator x f = reserved x >> pure f
+-- Binary operators
+wrapBinaryOperator :: String -> (a -> a -> a) -> ReadP (a -> a -> a)
+wrapBinaryOperator x f = reserved x >> pure f
 
-parseAdd :: ReadP AST
-parseAdd = opAdd <$> parseOperand <*> (char '+' *> parseOperand)
+parseExp :: ReadP (AST -> AST -> AST)
+parseExp = wrapBinaryOperator "e" opExp
+  where
+    opExp = \a b -> Operator $ BinaryOperator $ Sub a b
+
+parsePow :: ReadP (AST -> AST -> AST)
+parsePow = wrapBinaryOperator "^" opPow
+  where
+    opPow = \a b -> Operator $ BinaryOperator $ Pow a b
+
+parseMul :: ReadP (AST -> AST -> AST)
+parseMul = wrapBinaryOperator "*" opMul
+  where
+    opMul = \a b -> Operator $ BinaryOperator $ Mul a b
+
+parseDiv :: ReadP (AST -> AST -> AST)
+parseDiv = wrapBinaryOperator "/" opDiv
+  where
+    opDiv = \a b -> Operator $ BinaryOperator $ Div a b
+
+parseMod :: ReadP (AST -> AST -> AST)
+parseMod = wrapBinaryOperator "%" opMod
+  where
+    opMod = \a b -> Operator $ BinaryOperator $ Mod a b
+
+parseAdd :: ReadP (AST -> AST -> AST)
+parseAdd = wrapBinaryOperator "+" opAdd
   where
     opAdd = \a b -> Operator $ BinaryOperator $ Add a b
 
 parseSub :: ReadP (AST -> AST -> AST)
-parseSub = parseOperator "-" opSub
+parseSub = wrapBinaryOperator "-" opSub
   where
     opSub = \a b -> Operator $ BinaryOperator $ Sub a b
 
-parseAddAndSub :: ReadP (AST -> AST -> AST)
-parseAddAndSub = parseOperator "+" opAdd <|> parseOperator "-" opSub
+parseGt :: ReadP (AST -> AST -> AST)
+parseGt = wrapBinaryOperator ">" opGt
   where
-    opAdd = \a b -> Operator $ BinaryOperator $ Add a b
-    opSub = \a b -> Operator $ BinaryOperator $ Sub a b
+    opGt = \a b -> Operator $ BinaryOperator $ Gt a b
 
-parseMulAndDiv :: ReadP (AST -> AST -> AST)
-parseMulAndDiv = parseOperator "*" opMul <|> parseOperator "/" opDiv <|> parseOperator "%" opMod
+parseLt :: ReadP (AST -> AST -> AST)
+parseLt = wrapBinaryOperator "<" opLt
   where
-    opMul = \a b -> Operator $ BinaryOperator $ Mul a b
-    opDiv = \a b -> Operator $ BinaryOperator $ Div a b
-    opMod = \a b -> Operator $ BinaryOperator $ Mod a b
+    opLt = \a b -> Operator $ BinaryOperator $ Lt a b
 
+parseGe :: ReadP (AST -> AST -> AST)
+parseGe = wrapBinaryOperator ">=" opGe
+  where
+    opGe = \a b -> Operator $ BinaryOperator $ Ge a b
+
+parseLe :: ReadP (AST -> AST -> AST)
+parseLe = wrapBinaryOperator "<=" opLe
+  where
+    opLe = \a b -> Operator $ BinaryOperator $ Le a b
+
+parseEq :: ReadP (AST -> AST -> AST)
+parseEq = wrapBinaryOperator "==" opEq
+  where
+    opEq = \a b -> Operator $ BinaryOperator $ Eq a b
+
+parseNe :: ReadP (AST -> AST -> AST)
+parseNe = wrapBinaryOperator "!=" opNe
+  where
+    opNe = \a b -> Operator $ BinaryOperator $ Ne a b
+
+parseAnd :: ReadP (AST -> AST -> AST)
+parseAnd = wrapBinaryOperator "&&" opAnd
+  where
+    opAnd = \a b -> Operator $ BinaryOperator $ And a b
+
+parseOr :: ReadP (AST -> AST -> AST)
+parseOr = wrapBinaryOperator "||" opOr
+  where
+    opOr = \a b -> Operator $ BinaryOperator $ Or a b
+
+parsePowExpr :: ReadP (AST -> AST -> AST)
+parsePowExpr = parsePow <|> parseExp
+
+parseMulDivMod :: ReadP (AST -> AST -> AST)
+parseMulDivMod = parseMul <|> parseDiv <|> parseMod
+
+parseAddSub :: ReadP (AST -> AST -> AST)
+parseAddSub = parseAdd <|> parseSub
+
+parseGtLtGeLeEqNe :: ReadP (AST -> AST -> AST)
+parseGtLtGeLeEqNe = parseGt <|> parseLt <|> parseGe <|> parseLe <|> parseEq <|> parseNe
+
+-- 70
 parseExpr :: ReadP AST
-parseExpr = parseTerm `chainl1` parseAddAndSub
+parseExpr = parseTerm75 `chainl1` parseOr
 
-parseTerm :: ReadP AST
-parseTerm = parseFactor `chainl1` parseMulAndDiv
+-- 75
+parseTerm75 :: ReadP AST
+parseTerm75 = parseTerm80 `chainl1` parseAnd
+
+-- 80
+parseTerm80 :: ReadP AST
+parseTerm80 = parseTerm95 `chainl1` parseGtLtGeLeEqNe
+
+-- 95
+parseTerm95 :: ReadP AST
+parseTerm95 = parseTerm100 `chainl1` parseAddSub
+
+-- 100
+parseTerm100 :: ReadP AST
+parseTerm100 = parseTerm120 `chainl1` parseMulDivMod
+
+-- 120
+parseTerm120 :: ReadP AST
+parseTerm120 = parseFactor `chainl1` parsePowExpr
 
 parseFactor :: ReadP AST
 parseFactor = parseOperand <|> parens parseExpr
